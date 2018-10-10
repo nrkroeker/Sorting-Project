f422acea299941bc833ce6ea782b587809c98af1
Nathalie Kroeker <nathaliekroeker@gmail.com>
2018-10-09 21:30:53 -0400

    Add comments

--- sort.pl
+++ sort.pl
@@ -1,11 +1,14 @@
+# Array to store user-inputted integers
 @numbers = ();
 
+# Method to print all the numbers in the array
 sub printNumbers {
     foreach my $n (@numbers) {
         print "$n ";
     }
 }
 
+# Method to sort the numbers using Bubble sort
 sub sortList {
     $length = scalar @numbers;
     for $i (0 .. $length) {
@@ -19,16 +22,19 @@ sub sortList {
     }
 }
 
+# Retrieve input from the user and push it into the array
 print "Please input 10 integers\n";
 for $x (0 .. 9) {
     print ">";
     my $newNumber = <STDIN>;
-    chomp $newNumber;
+    chomp $newNumber; # Trim the newline from the user pressing enter
     push @numbers, $newNumber;
 }
+# Print the list of integers as inputted by the user
 print "\nUnsorted list: ";
 printNumbers();
 
+# Sort the integers, and print them in sorted order
 sortList();
 print "\nSorted list: ";
 printNumbers();
--- sort.py
+++ sort.py
@@ -1,3 +1,4 @@
+# Method to sort an array of integers using Bubble sort
 def sortList(numbers):
     size = len(numbers)
 
@@ -8,19 +9,24 @@ def sortList(numbers):
                 numbers[j] = numbers[j + 1]
                 numbers[j + 1] = temp
 
+# Method to print the array of integers on one line
 def printIntArray(arr):
     for i in range(len(arr)):
         print ("%d" %arr[i], end=" ")
 
 def main():
+    # Get 10 integers from the user and append them to an array
     print("Please input 10 integers")
     numbers = [];
     for x in range(10):
         newNumber = input("> ")
         numbers.append(int(newNumber))
+
+    # Print the array as it was inputted
     print("\n\nUnsorted List: ", end="")
     printIntArray(numbers)
 
+    # Sort the array and print it in sorted order
     sortList(numbers)
     print ("\n\nSorted List: ", end="")
     printIntArray(numbers)
